532451e533d989cb490c7d866b9f3577929aac0b
Cherrelle-Ward <Cherrelle_Ward@hotmail.co.uk>
2022-05-23 17:44:18 +0100

    added pricing data

--- src/components/artwork.jsx
+++ src/components/artwork.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import react from "react";
 import "../App.css";
+import { faker } from "@faker-js/faker";
 
 const Artwork = ({
   error,
@@ -10,7 +11,7 @@ const Artwork = ({
   setIiifUrl,
   iiifUrl,
 }) => {
-  //! Add Handle
+  const [moreInfo, setMoreInfo] = useState(false);
 
   let pageNum = Math.floor(Math.random() * 5000) + 1;
 
@@ -19,7 +20,7 @@ const Artwork = ({
     try {
       setError(false);
       const res = await fetch(
-        `https://api.artic.edu/api/v1/artworks?fields=id,artist_title,title,artist_display,image_id&limit=10&page=${pageNum}`
+        `https://api.artic.edu/api/v1/artworks?fields=id,artist_title,title,artist_display,image_id&limit=12&page=${pageNum}`
       );
       if (res.status !== 200) {
         throw new Error("oops");
@@ -43,23 +44,42 @@ const Artwork = ({
   }, []);
 
   return (
-    <div className="tile-container">
-      {imageId.map((item, index) => {
-        return (
-          <div className="tile">
-            <h1> {item.title} </h1>
-            <h2>By {item.artist_title}</h2>
-            <p>{item.artist_display}</p>
-            <img
-              key={index}
-              src={`${iiifUrl}/${item.image_id}/full/843,/0/default.jpg`}
-              alt="Artwork"
-            />{" "}
-          </div>
-        );
-      })}
+    <div className="main-container">
+      <div className="main-tile-container">
+        {imageId.map((item, index) => {
+          return (
+            <div className="tile">
+              <h1> {item.title} </h1>
+              <h2>By {item.artist_title}</h2>
+              <p>{item.artist_display}</p>
+              <p>
+                Price : £
+                {faker.datatype.number({
+                  min: 100,
+                  max: 10000,
+                })}
+              </p>
+              <img
+                key={index}
+                src={`${iiifUrl}/${item.image_id}/full/843,/0/default.jpg`}
+                alt="Artwork"
+              />
+              <button
+                onClick={() => {
+                  setMoreInfo(true);
+                }}
+              >
+                More Info
+              </button>
+              {!moreInfo ? null : <div>hello</div>}
+            </div>
+          );
+        })}
+      </div>
 
-      <button onClick={fetchImg}>Next Page</button>
+      <div className="btn">
+        <button onClick={fetchImg}>Next Page</button>
+      </div>
     </div>
   );
 };
